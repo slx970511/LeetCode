4ed4b98f9c5921babb91f83f097d1239210a8e5f
slx970511 <347343126@qq.com>
2017-07-26 13:17:40 +0800

    Delete twoSum.js

--- twoSum.js
+++
@@ -1,6 +0,0 @@
-var twoSum = function(nums, target) {
-    for(let i=0;i<nums.length;i++){
-        if(nums.indexOf(target - nums[i])>-1 &&nums.indexOf(target - nums[i])!==i)
-            return [i,nums.indexOf(target - nums[i])];
-    }
-};
